2319499b777fd0ff154206d56789be1a84df18d0
tchoutdara <monkeymilitia444@gmail.com>
2018-08-29 15:37:47 -0600

    add ruby vowel counter, add js palindrome checker

--- js-challenge.js
+++ js-challenge.js
@@ -1,7 +1,27 @@
 // Write a script that checks whether a passed string is palindrome or not.
-function arrange() {
-    var input = "user input"
-    return input.split('').sort().join('')
-    console.log(input)
+// var input = "user input"
+
+// function arrange() {
+//     sort = input.split('').sort().join('')
+ 
+//    return console.log(sort)
+// }
+
+// arrange()
+
+//Palindrome checker
+
+var input = "race car"
+
+function palindrome() {
+    reverse = input.split('').reverse().join('')
+ 
+   
+   if (input.replace(/\s+/g, '') === reverse.replace(/\s+/g, '')){
+     return console.log('Its a palindrome')
+   }else{
+     return console.log('It is not a palindrome')
+     }
 }
-arrange()
+
+palindrome()
--- rb-challenge.rb
+++ rb-challenge.rb
@@ -8,13 +8,28 @@
 # arrange
 
 # Write a script that checks whether a passed string is palindrome or not.
-def palindrome
-    input = gets.to_s.strip.gsub(" ", "")
-    reverse = input.reverse
-    if (input === reverse)
-        puts "It is a palindrome"
-    else
-        puts "It is not a palindrome"
-    end
-end
-palindrome
+# def palindrome
+#     input = gets.to_s.strip.gsub(" ", "")
+#     reverse = input.reverse
+#     if (input === reverse)
+#         puts "It is a palindrome"
+#     else
+#         puts "It is not a palindrome"
+#     end
+# end
+# palindrome
+
+    # def count_vowels()
+    #     string = gets.to_s.strip
+    #     vowels = 0
+    #     counter = 0
+    #     while counter < string.length do
+    #       if string[counter]=="a" || string[counter]=="e" || string[counter]=="i" || string[counter]=="o" || string[counter]=="u"
+    #         vowels += 1
+    #       end
+    #       counter += 1
+    #     end
+    #     puts vowels
+    # end
+
+    # count_vowels()
